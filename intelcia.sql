1ff896281730d20ae2005c4bbd09021e0913dff7
Said ZITOUNI <zitouni.sd@gmail.com>
2021-11-28 21:29:57 +0100

    start sql salarie,division,pole_competence

---
+++ intelcia.sql
@@ -0,0 +1,27 @@
+--division 
+CREATE TABLE division(
+    _numero SERIAL PRIMARY KEY,
+    nom varchar(255),
+    CA double precision,
+    ville varchar(255),
+    pays varchar(255)
+);
+-- pole competence
+CREATE TABLE pole_competence(
+    _numero SERIAL PRIMARY KEY,
+    nom varchar(255),
+    nb_salarie integer
+);
+--salarié
+CREATE TABLE salarie(
+    _matricule bigint PRIMARY KEY,
+    prenom varchar(255),
+    nom varchar(255),
+    salaire integer,
+    comission double precision,
+    ville var(255),
+    code_postal varchar(255),
+    pays varchar(255),
+    pole_competence_id REFERENCES pole_competence(_numero),
+    division_id REFERENCES division(_numero)
+); 
